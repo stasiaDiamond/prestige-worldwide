7d3b1f705b5d6461953c8a1aea2504081eab7974
stasiaDiamond <nsisco@uw.edu>
2023-05-30 16:13:54 -0700

    backend file structure

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
+        <img src="" className="" alt="Only Hands logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
