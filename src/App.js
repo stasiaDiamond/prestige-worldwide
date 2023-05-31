65e540fce5a63fd84d74ecbb45d0e3ffb2d1faea
Cyndi Fosmark <cmfosmark@gmail.com>
2023-05-31 11:34:59 -0700

    idk what this is

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="" className="" alt="Only Hands logo" />
+        <img src="./images/transparent-logo.png" className="" alt="Only Hands logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
